8f4946ae5d2bb4dd04a0e5429f8aae460913248c
MohamedAliBouzaiene <mohamedali.bouzaiene@esprit.tn>
2022-02-21 18:18:51 +0100

    changed some visual views

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Security;
@@ -82,7 +83,9 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         if (!$this->csrfTokenManager->isTokenValid($token)) {
             throw new InvalidCsrfTokenException();
         }
-        if (!$user->getIsEnabled() || $user->getOauth() == true)
+        if (!$user->getIsEnabled())
+            throw new AuthenticationException("This account is locked!");
+        if ($user->getOauth() == true)
             return false;
         return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
     }
